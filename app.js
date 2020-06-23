4051f9355941a0efb897c903971e4d17c5ac8b23
Achmad Fityan <achmadfityan@Achmads-MacBook-Pro.local>
2020-06-23 19:22:10 +0700

    coba build otomatis

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send(' hellooo faiz');
+  res.send(' hellooo kamilah');
 });
  
 app.listen(process.env.PORT || 5000);
